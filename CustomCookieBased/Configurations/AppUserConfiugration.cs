f6b1b2682ca089a64cca4e1b450e53e6be811ef2
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-23 20:53:41 +0300

    Configurations & migrations & context & startup updated

---
+++ CustomCookieBased/Configurations/AppUserConfiugration.cs
@@ -0,0 +1,35 @@
+﻿using CustomCookieBased.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCookieBased.Configurations
+{
+    public class AppUserConfiugration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(x => x.Password).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Username).HasMaxLength(250).IsRequired();
+        }
+    }
+    public class AppRoleConfiugration : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.Property(x => x.Definition).HasMaxLength(200).IsRequired();
+        }
+    }
+    public class AppUserRoleConfiugration : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            builder.HasKey(x => new { x.UserId, x.RoleId });
+            builder.HasOne(x => x.AppRole).WithMany(x => x.AppUserRoles).HasForeignKey(x => x.RoleId);
+            builder.HasOne(x => x.AppUser).WithMany(x => x.AppUserRoles).HasForeignKey(x => x.UserId);
+        }
+    }
+}
---
+++ CustomCookieBased/Data/CustomCookieContext.cs
@@ -0,0 +1,26 @@
+﻿using CustomCookieBased.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCookieBased.Data
+{
+    public class CustomCookieContext : DbContext
+    {
+        public CustomCookieContext(DbContextOptions<CustomCookieContext> options):base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<AppRole> Roles { get; set; }
+        public DbSet<AppUserRole> UserRoles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppUserConfiugration());
+            modelBuilder.ApplyConfiguration(new AppRoleConfiugration());
+            modelBuilder.ApplyConfiguration(new AppUserRoleConfiugration());
+        }
+    }
+}
