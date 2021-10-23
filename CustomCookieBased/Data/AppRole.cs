6c594fba0dc65decce2be7c1b0920881fb504586
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-23 20:33:42 +0300

    Add project files.

---
+++ CustomCookieBased/Data/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCookieBased.Data
+{
+    public class AppRole
+    {
+        public int Id { get; set; }
+        public string Definition { get; set; }
+        public List<AppUserRole> AppUserRoles { get; set; }
+    }
+}
---
+++ CustomCookieBased/Data/AppUserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCookieBased.Data
+{
+    public class AppUserRole
+    {
+        public int UserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public int RoleId { get; set; }
+        public AppRole AppRole { get; set; }
+        
+    }
+}
